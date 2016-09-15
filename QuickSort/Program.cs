63e9d8b48a6e3eb928aff73cd8643d07c9f62e9c
Ирина Новикова <pryanyi.santehnik@gmail.com>
2016-09-15 21:06:36 +0300

    2

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,6 +10,32 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
+
         }
+
+        int Partition<T>(T[] arrayToSort, int indexOfFirstElement, int indexOfLastElement) where T : IComparable<T>
+        {
+            int i = indexOfFirstElement;
+            for (int j = indexOfFirstElement; j <= indexOfLastElement; j++)
+            {
+                if (arrayToSort[j].CompareTo(arrayToSort[indexOfLastElement]) <= 0)
+                {
+                    T temp = arrayToSort[i];
+                    arrayToSort[i] = arrayToSort[j];
+                    arrayToSort[j] = temp;
+                    i++;
+                }
+            }
+            return i - 1;
+        }
+
+        void QuickSort<T>(T[] arrayToSort, int indexOfFirstElement, int indexOfLastElement) where T : IComparable<T>
+        {
+            if (indexOfFirstElement >= indexOfLastElement) return;
+            int c = Partition(arrayToSort, indexOfFirstElement, indexOfLastElement);
+            QuickSort(arrayToSort, indexOfFirstElement, c - 1);
+            QuickSort(arrayToSort, c + 1, indexOfLastElement);
+        }
+
     }
 }
